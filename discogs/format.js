5e1b9a91d3245af2b0bb047c8da74667d3110376
Adam Smith <adamcsmith@gmail.com>
2014-07-01 22:02:36 -0400

    latest stacked bar chart stuff

--- discogs/format.js
+++ discogs/format.js
@@ -1,24 +1,27 @@
+'use strict';
+
 var redis = require('redis'),
   client = redis.createClient(6379, '127.0.0.1'),
   q = require('q'),
   fs = require('fs'),
-  cache = [],
-  releases = 
+  redisClientGet = q.nbind(client.get, client),
   REDIS_KEY_SET_PAGE = 'discogs:page:',
-  REDIS_KEY_LIST_KEYS = 'discogs:keys';
-
-redisClientGet = q.nbind(client.get, client);
+  REDIS_KEY_LIST_KEYS = 'discogs:keys',
+  REDIS_KEY_HASH_YEAR = 'discogs:year';
 
 client.on('ready', function() {
   console.log('REDIS ready...');
-  getDiscogsData();
+  getDiscogsData(redisCallback);
 });
 
+/**
+ * Need a way to handle async in a more dynamic way to get this to work.
+ */
 function getDiscogsDataBak() {
+  var cache;
   client.llen(REDIS_KEY_LIST_KEYS, function(err, reply) {
     for (var i = 0; i < reply; i++) {
       client.lindex(REDIS_KEY_LIST_KEYS, i, function(err, reply) {
-        console.log(reply);
         client.get(reply, function(err, reply) {
           console.log(reply);
           cache.push(reply);
@@ -29,9 +32,11 @@ function getDiscogsDataBak() {
 }
 
 /**
- * This is ridonkulous. There's a better way.
+ * This is ridonkulous. There's a better way...
+ * @param {function} cb callback to be called after promise is resolved
+ * @return undefined
  */
-function getDiscogsData() {
+function getDiscogsData(cb) {
   q.all([
     redisClientGet('discogs:page:1'),
     redisClientGet('discogs:page:2'),
@@ -56,60 +61,132 @@ function getDiscogsData() {
     redisClientGet('discogs:page:21'),
     redisClientGet('discogs:page:22')
   ]).then(function(data) {
-    data.forEach(function(el, i, arr) {
-      var data = JSON.parse(el);
-      var releases = data.releases;
-      cache.push.apply(cache, releases);
-    });
-    var results = {
-      releases: cache
-    };
-    var aggregated = aggregateReleaseYears(results.releases);
-    saveAggregated(JSON.stringify(aggregated));
+    cb(data);
   });
 }
 
+function redisCallback(response) {
+  var releasesArr = extractReleasesData(response);
+  var results = {
+    releases: releasesArr
+  }
+  var aggregated = aggregateReleaseData(results.releases);
+  saveAggregated(JSON.stringify(aggregated));
+}
+
 /**
- * rebuild formatted object from discogs json data source
- * @param {object} data the discogs json source
- * @returns {object}
+ * Build array of releases objects for easier aggregation of years info
+ * @param {array} array array of discogs json objects from redis
+ * @return {array}
  */
-function aggregateReleaseYears(data) {
+function extractReleasesData(array) {
+  var cache = [];
+  array.forEach(function(el, i, arr) {
+    var data = JSON.parse(el);
+    var releases = data.releases;
+    cache.push.apply(cache, releases);
+  });
+  return cache;
+}
+
+/**
+ * Write file to disk.
+ * @param {object} obj the json object to save in the file
+ * @return undefined
+ */
+function saveAggregated(obj) {
+  var store = {};
+
+  //save to redis.
+  //redis format:
+  //'discogs:year:2001' Vinyl 181 CD 10 DVD 0 'No Format Specified' 0 total 191
+
+  obj = JSON.parse(obj);
+  for (var prop in obj) {
+    var key = REDIS_KEY_HASH_YEAR + ':' + prop,
+      tots = obj[prop]['total'],
+      vinyl = obj[prop]['Vinyl'] || 0,
+      cd = obj[prop]['CD'] || 0,
+      noformat = obj[prop]['No Format Specified'] || 0,
+      dvd = obj[prop]['DVD'] || 0,
+      hash = {};
+
+    hash = {
+      'total': tots,
+      'Vinyl': vinyl,
+      'CD': cd,
+      'No Format Specified': noformat,
+      'DVD': dvd
+    };
+
+    saveHash(key, hash);
+    store[prop] = hash;
+  }
+
+  function saveHash(k, v) {
+    client.hmset(k, v, function(err, reply) {
+      if (err) console.log(err);
+      console.log('REDIS saved aggregated', k, v);
+    });
+  }
+
+  var fileName = 'discogs_aggregated_years_reformatted.json';
+
+  fs.writeFile(fileName, JSON.stringify(store), function(err) {
+    if (err) throw err;
+    console.log('Saved aggregated file', fileName);
+  });
+
+}
+
+/**
+ * Extract release year and format data. Reformat it into
+ * a cute little condensed object and return it.
+ * @param {object} data the discogs json
+ * @return {object} summed the cute little condensed object
+ */
+function aggregateReleaseData(data) {
   var summed = {},
-    years = [];
+    condensed = [];
 
   //extract the release years into an array
   data.forEach(function(el, i, arr) {
-    years.push(el.basic_information.year);
+    var obj = {},
+      formats;
+
+    formats = el.basic_information.formats;
+
+    if (formats.length === 0) {
+      formats = [{name: 'No Format Specified'}];
+    }
+    if (formats.length > 1) {
+      formats = [formats.shift()];
+    }
+
+    obj.year = el.basic_information.year;
+    obj.format = formats[0].name;
+
+    condensed.push(obj);
   });
 
-  //sum number of times each year occurs
-  years.map(function(year) {
+  condensed.map(function(release) {
+    var year = release.year,
+      format = release.format;
+
     if (year in summed) {
-      summed[year]++;
+      summed[year]['total']++;
     } else {
-      summed[year] = 1;
+      summed[year] = {
+        total: 1
+      }
+    }
+
+    if (format in summed[year]) {
+      summed[year][format]++;
+    } else {
+      summed[year][format] = 1;
     }
   });
 
   return summed;
-}
-
-function saveAggregated(obj) {
-  fs.writeFile('discogs_aggregated_years.json', obj, function(err) {
-    if (err) throw err;
-    console.log('Saved aggregated.');
-  });
-}
-
-/**
- * format data into array of objects
- */
-function formatSummed(summed) {
-  for (var prop in summed) {
-    var obj = {};
-    obj.year = prop;
-    obj.count = summed[prop];
-    results.push(obj);
-  }
 }
